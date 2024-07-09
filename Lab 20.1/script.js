ad40341ed39b4beb68db42cc50b25e0c65def849
lanhoang5 <lanhoang16102000@gmail.com>
2024-07-10 00:37:45 +0700

    20 start

---
+++ Lab 20.1/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close");
+const btnsOpenModal =
+  document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("button click");
+  });
+}
