a2139f8fd3f8379db6ab400d3c04e233d07b4d44
lanhoang5 <lanhoang16102000@gmail.com>
2024-07-11 14:29:14 +0700

    20.1 done

--- Lab 20.1/script.js
+++ Lab 20.1/script.js
@@ -1,12 +1,32 @@
 "use strict";
+// Project Cửa sổ modal
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close");
+const btnCloseModal =
+  document.querySelector(".close-modal");
 const btnsOpenModal =
   document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+// làm việc với class
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("button click");
-  });
+  btnsOpenModal[i].addEventListener("click", openModal);
 }
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+// Xử lý "Esc" Keypress Event
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "Escape" &&
+    !modal.classList.contains("hidden")
+  ) {
+    closeModal();
+  }
+});
